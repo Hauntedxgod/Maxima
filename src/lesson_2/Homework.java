4e43d6569970ee3bee1bd314fc68f7030f2b270b
Hauntedxgod <kamaldamir2016@mail.ru>
2023-10-10 19:25:09 +0300

    Maxima

--- src/lesson_2/Homework.java
+++ src/lesson_2/Homework.java
@@ -3,73 +3,64 @@ package lesson_2;
 
 
 public class Homework {
-//    public static int a = 1;
-//    public static int b = 3;
-//    public static int c = 9;
-//    public static int d = 27;
-//      public static void main(String[] args) {
+    public static void main(String[] args){
 
-//        int result = -a + b - c + d;
-
-//        System.out.println(result);
-
-    // Пусть цена товара A обычно составляет 1000 руб, и товара B составляет 500 руб.
-    // Если клиент покупает товары вместе, то на них действует скидка 10% на всю покупку.
-
-//    public static void main (String[] args) {
-//        double a = 1000.0 ;
-
-//        double b = 500.0 ;
+     int a = 1;
+     int b = 3;
+     int c = 9;
+     int d = 27;
 
 
 
-//        double general = a + b  ;
-//        System.out.println("Сумма в общем - " + general);
-//        double purchase = (a + b)  - ((a + b) * 10) / 100 ;
-//        System.out.println("Сумма покупки со скидкой - " + purchase);
-//        double discount = (a + b) / 10;
-//        System.out.println("Сумма скидки - " + discount);
+        int result = -a + b - c + d;
 
- //   public static void main (String[] args) {
- //       double rub = 200;
- //       double rub2 = 30;
+        System.out.println(result);
 
- //   double took =  (10 * rub) - ((10 * rub) / 100) * 15 + (2 * rub2);
+        // Пусть цена товара A обычно составляет 1000 руб, и товара B составляет 500 руб.
+        // Если клиент покупает товары вместе, то на них действует скидка 10% на всю покупку.
 
 
+            double a1 = 1000.0;
+
+            double b1 = 500.0;
 
 
-//        System.out.print("Итоговая цена - " + took  );
-
-//    public static void main (String[] args) {
-//        double tovar1 = 500;
-//        double tovar2 = 50;
-
-//        double itogtovara = (tovar1 * 7) - (( tovar1 * 7 ) / 100 ) * 50 + ( 1 * tovar2 ) ;
-
-//        System.out.println("Итоговая цена - " + itogtovara);
+            double general = a1 + b1;
+            System.out.println("Сумма в общем - " + general);
+            double purchase = (a1 + b1) - ((a1 + b1) * 10) / 100;
+            System.out.println("Сумма покупки со скидкой - " + purchase);
+            double discount = (a1 + b1) / 10;
+            System.out.println("Сумма скидки - " + discount);
 
 
+            double rub = 200;
+            double rub2 = 30;
 
-    public static void main (String[] args) {
-
-        int apple1 = 300;
-        int onion = 20;
-
-        int itog = (apple1 * 2 ) - ((apple1 * 2 ) / 100  ) * 10 + (onion * 3);
-
-        System.out.println("Итог всего - " + itog);
+            double took = (10 * rub) - ((10 * rub) / 100) * 15 + (2 * rub2);
 
 
+            System.out.println("Итоговая цена - " + took);
 
 
+            double tovar1 = 500;
+            double tovar2 = 50;
+            double itogtovara = (tovar1 * 7) - ((tovar1 * 7) / 100) * 50 + (1 * tovar2);
+
+            System.out.println("Итоговая цена - " + itogtovara);
 
 
+            int apple1 = 300;
+            int onion = 20;
+
+            int itog = (apple1 * 2) - ((apple1 * 2) / 100) * 10 + (onion * 3);
+
+            System.out.println("Итог всего - " + itog);
 
 
-
+        }
     }
-}
+
+
 
 
 
